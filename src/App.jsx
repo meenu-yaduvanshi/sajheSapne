d1ba8d02e124da4106a7845769da2a6f677d212a
Simran <simrantamang6008@gmail.com>
2024-09-24 10:09:59 +0530

    added nav bar and home content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+
 import './App.css'
 import Nav from './components/Nav'
-import Home from './components/Home'
+import * as React from 'react';
+import Home from './components/Home';
 
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-  <Nav/>
-  <Home/>
+      <Nav />
+      <Home/>
+
     </>
   )
 }
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,12 +5,18 @@ function Nav() {
     return (
         <>
             <nav className="navbar">
-                <a href="#">Power of Learning</a>
-                <a href="#">Power of Earning</a>
-                <a href="#">Power of Collective</a>
-                <a href="#">Agency Buildingg</a>
-                <a href="#">Get in Touch</a>
+                <img id='logo' src="../src/assets/logo.png" alt="logo" />
+                <div className='links-container'>
+                    <a href="#"><p>Power of <br /><span>Learning</span></p></a> <p>+</p>
+                    <a href="#"><p>Power of <br /><span>Earning</span></p></a><p>+</p>
+                    <a href="#"><p>Power of <br /><span>Collective</span></p></a><p>=</p>
+                    <a href="#"><p id='agency-building'>Agency <br />Building</p></a>
+                </div>
+
+                <a href="#" id='anchor-g-i'><p id='get-involved'>Get<br />Involved</p></a>
+                <img id='search-bar' src="../src/assets/searchicon.png" alt="" />
             </nav>
+
         </>
     )
 
