03e0fcf318705e22447ab0261aa71cb60921cdf7
meenu yadav <meenuyaduvanshi24@gmail.com>
2024-10-01 14:19:04 +0530

    complete footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,42 +7,59 @@ const Footer = () => {
         <>
             <footer>
                 <div className='newsletter'>
-                    <h3>Sign up for our Newsletter</h3>
+                    <h3>Join our Newsletter</h3>
                     <form>
-                        <input type="email" placeholder="Your email address" class="email-input" />
-                        <button type="submit" class="submit-btn">Subscribe</button>
+                        <input type="email" placeholder="Your email address" className="email-input"/>
+                        {/* <button type="submit" className="submit-btn">Subscribe</button> */}
+                        <button class="submit-btn" type='submit'>Subscribe</button>
                     </form>
                 </div>
+                <hr className='hr' />
 
-                <div className='links'>
-                    <ul>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">Work</a></li>
-                        <li><a href="#">Impact</a></li>
-                        <li><a href="#">Success stories</a></li>
-                    </ul>
-                </div>
                 <div className='contact-container'>
                     <div>
-                        {/* <p className='contact-us'>Contact Us</p> */}
+                        <p>Reach out to us at</p>
                         <ul>
-                            <li className='contact-us'>Contact Us</li>
                             <li>sapnewaalis@gmail.com</li>
                             <li>+91- 80910 93301</li>
                         </ul>
                     </div>
-
-                    {/* <div className='contact'>
-                        <a href="#" className="fa fa-linkedin"></a>
-                        <a href="#" className="fa fa-youtube"></a>
-                        <a href="#" className="fa fa-instagram"></a>
-                    </div> */}
                 </div>
-                <hr className='hr'/>
+                <hr className='hr' />
+                <div className='links'>
+                    <p>LINKS</p>
+                    <ul>
+                        <li><a href="#">Power of Learning</a></li>
+                        <li><a href="#">Power of Earning</a></li>
+                        <li><a href="#">Power of Collective</a></li>
+                        <li><a href="#">Idea of Freedom</a></li>
+                    </ul>
+                </div>
+                <hr className='hr' />
+                <div className='links'>
+                    <p>NETWORKING</p>
+                    <ul>
+                        <li><a href="#">Sajhe Sapne Team</a></li>
+                        <li><a href="#">Careers at Sajhe Sapne</a></li>
+                    </ul>
+                </div>
+                <hr className='hr' />
+                <div className='links'>
+                    <p>GET INVOLVED</p>
+                    <ul>
+                        <li><a href="#">Start a Sapne Center</a></li>
+                        <li><a href="#">Become a Patron for a Sapnewaali</a></li>
+                        <li><a href="#">Support Sajhe's Pedagogy Tools/Games</a></li>
+                        <li><a href="#">Become a Ally</a></li>
+                    </ul>
+                </div>
 
+
+
+                <hr className='hr' />
                 <div className='copyrights'>
                     <ul>
-                        <li className="no-bullet"   > <a href="#">Privacy Policy</a></li>
+                        <li className="no-bullet"> <a href="#">Privacy Policy</a></li>
                         <li> <a href="#">Terms and Conditions</a></li>
                         <li> <a href="#">Charity Numbers</a></li>
                     </ul>
@@ -52,4 +69,3 @@ const Footer = () => {
     )
 }
 export default Footer
-
