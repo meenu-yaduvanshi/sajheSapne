95b78d99f3800482953d55f17f8df9a1f0cdac4d
meenu yadav <meenuyaduvanshi24@gmail.com>
2024-10-01 14:08:47 +0530

    complete foter in media slideshow branch

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import Nav from './components/Nav'
 import * as React from 'react';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import MediaSlideshow from './components/MediaSlideshowHome';
 
 
 function App() {
   return (
     <>
       <Nav />
-      <Home/>
-<Footer/>
+      <Home />
+      <MediaSlideshow />
+      <Footer />
     </>
   )
 }
