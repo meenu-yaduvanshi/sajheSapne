c414c2b6950279eff621e7214ae26522fd49464d
meenu yadav <meenuyaduvanshi24@gmail.com>
2024-10-18 14:27:01 +0530

    footer make responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,6 +6,7 @@ const Footer = () => {
     return (
         <>
             <div className='ft-illustratuin-div'>
+                <img src="newfooter.png" alt="" id='footer-big-ills' />
                 <img src="cropfooter.png" alt="footer-illustration" id='footer-ills' />
             </div>
             <footer>
@@ -13,66 +14,67 @@ const Footer = () => {
                     <h3>Join our Newsletter</h3>
                     <form>
                         <input type="email" placeholder="Your email address" className="email-input" />
-                        {/* <button type="submit" className="submit-btn">Subscribe</button> */}
                         <button className="submit-btn" type='submit'>Subscribe</button>
                     </form>
                 </div>
                 <hr className='hr' />
 
-                <div className='contact-container'>
-                    <div>
-                        <p>Reach out to us at</p>
-                        <ul>
-                            <li>sapnewaalis@gmail.com</li>
-                            <li>+91- 80910 93301</li>
-                        </ul>
-                    </div>
+                {/* <section className='ft-big-section'> */}
+                    <div className='contact-container'>
+                        <div>
+                            <p>REACH OUT TO US AT</p>
+                            <ul>
+                                <li>sapnewaalis@gmail.com</li>
+                                <li>+91- 80910 93301</li>
+                            </ul>
+                        </div>
 
-                    {/* <div className='ft-icon-div'>
+                        {/* <div className='ft-icon-div'>
                         <a href=""> <img src="footerImg/youtube.png" alt="" id='youtube' className='ft-icon' />  </a>
                         <a href=""> <img src="footerImg/instagram4.png" alt="" id='instagram' className='ft-icon' /> </a>
                         <a href="">  <img src="footerImg/linkedin2.png" alt="" id='linkedin' className='ft-icon' /> </a>
                     </div> */}
 
-                    {/* <div>
+                        {/* <div>
                         <i class="fa fa-instagram"></i>
                         <a href=""> <i class="fa fa-linkedin"></i></a>
                         <a href=""> <i class="fa fa-youtube"></i></a>
                     </div> */}
-                </div>
+                    </div>
+
+                    <hr className='hr' />
+                    <div className='links'>
+                        <p>LINKS</p>
+                        <ul>
+                            <li><a href="#">Power of Learning</a></li>
+                            <li><a href="#">Power of Earning</a></li>
+                            <li><a href="#">Power of Collective</a></li>
+                            <li><a href="#">Idea of Freedom</a></li>
+                        </ul>
+                    </div>
+                    <hr className='hr' />
+                    <div className='links'>
+                        <p>NETWORKING</p>
+                        <ul>
+                            <li><a href="#">Sajhe Sapne Team</a></li>
+                            <li><a href="#">Careers at Sajhe Sapne</a></li>
+                        </ul>
+                    </div>
+                    <hr className='hr' />
+                    <div className='links'>
+                        <p>GET INVOLVED</p>
+                        <ul>
+                            <li><a href="#">Start a Sapne Center</a></li>
+                            <li><a href="#">Become a Patron for a Sapnewaali</a></li>
+                            <li><a href="#">Support Sajhe's Pedagogy Tools/Games</a></li>
+                            <li><a href="#">Become a Ally</a></li>
+                        </ul>
+                    </div>
+                {/* </section> */}
 
                 <hr className='hr' />
-                <div className='links'>
-                    <p>LINKS</p>
-                    <ul>
-                        <li><a href="#">Power of Learning</a></li>
-                        <li><a href="#">Power of Earning</a></li>
-                        <li><a href="#">Power of Collective</a></li>
-                        <li><a href="#">Idea of Freedom</a></li>
-                    </ul>
-                </div>
-                <hr className='hr' />
-                <div className='links'>
-                    <p>NETWORKING</p>
-                    <ul>
-                        <li><a href="#">Sajhe Sapne Team</a></li>
-                        <li><a href="#">Careers at Sajhe Sapne</a></li>
-                    </ul>
-                </div>
-                <hr className='hr' />
-                <div className='links'>
-                    <p>GET INVOLVED</p>
-                    <ul>
-                        <li><a href="#">Start a Sapne Center</a></li>
-                        <li><a href="#">Become a Patron for a Sapnewaali</a></li>
-                        <li><a href="#">Support Sajhe's Pedagogy Tools/Games</a></li>
-                        <li><a href="#">Become a Ally</a></li>
-                    </ul>
-                </div>
+<hr className="hr-for-desktop"/>
 
-
-
-                <hr className='hr' />
                 <div className='copyrights'>
                     <ul>
                         <li className="no-bullet"> <a href="#">Privacy Policy</a></li>
